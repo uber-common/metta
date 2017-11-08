b678cc29a25fef68e8cad6d66c6357bc63caf3f9
Chris Gates <cgates@uber.com>
2017-11-07 22:24:20 -0500

    mostly atomic testing ports

--- MITRE/Discovery/discovery_query_registry.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# MITRE PHASE: DISCOVERY
-# MITRE TECHNIQUE: Query Registry
-
-# Query Registry
-
-echo "Windows Command: REG QUERY HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs"
-vagrant winrm -c "REG QUERY HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs" #query a reg value from the command line
