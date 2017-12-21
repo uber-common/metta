bb10bee9738ff2c3523b6b058592505018cfe465
Chris Gates <cgates@uber.com>
2017-12-21 16:31:08 -0500

    remove db code b/c I removed the DB piece from the original project

--- workers/vagranttasks.py
+++ workers/vagranttasks.py
@@ -18,54 +18,6 @@ def alive_vagrant():
     return "pong"
 
 @sim_vagrant.task
-def runcmd_win(vagrant_cmd, rule_name, rule_uuid, hostname):
-	try:
-		print "##### DEBUG -- We made it to the vagrant function  -- DEBUG ###### "
-		print "'Running: {} with Rule GUID: {} against vagrant {}".format(vagrant_cmd, rule_uuid, hostname)
-		cmd = 'vagrant winrm {} -c "{}"'.format(hostname, vagrant_cmd)
-		args = shlex.split(cmd)
-		print args
-		dovagrant = subprocess.Popen(args)
-		print dovagrant
-		print "Inserting info into the DB"
-		execution_time = datetime.datetime.utcnow()
-		db.insert_row(rule_name, rule_uuid, hostname, execution_time)
-	except Exception as e:
-		print(e)
-
-@sim_vagrant.task
-def runcmd_osx(vagrant_cmd, rule_name, rule_uuid, hostname):
-	try:
-		print "##### DEBUG -- We made it to the vagrant function  -- DEBUG ###### "
-		print "'Running: {} with Rule GUID: {} against vagrant {}".format(vagrant_cmd, rule_uuid, hostname)
-		cmd = 'vagrant ssh {} -c "{}"'.format(hostname, vagrant_cmd)
-		args = shlex.split(cmd)
-		print args
-		dovagrant = subprocess.Popen(args)
-		print dovagrant
-		print "Inserting info into the DB"
-		execution_time = datetime.datetime.utcnow()
-		db.insert_row(rule_name, rule_uuid, hostname, execution_time)
-	except Exception as e:
-		print(e)
-
-@sim_vagrant.task
-def runcmd_linux(vagrant_cmd, rule_name, rule_uuid, hostname):
-	try:
-		print "##### DEBUG -- We made it to the vagrant function  -- DEBUG ###### "
-		print "'Running: {} with Rule GUID: {} against vagrant {}".format(vagrant_cmd, rule_uuid, hostname)
-		cmd = 'vagrant ssh {} -c "{}"'.format(hostname, vagrant_cmd)
-		args = shlex.split(cmd)
-		print args
-		dovagrant = subprocess.Popen(args)
-		print dovagrant
-		print "Inserting info into the DB"
-		execution_time = datetime.datetime.utcnow()
-		db.insert_row(rule_name, rule_uuid, hostname, execution_time)
-	except Exception as e:
-		print(e)
-
-@sim_vagrant.task
 def runcmd_nodb_win(vagrant_cmd, rule_name, rule_uuid, hostname):
 	try:
 		#print "changing locations"
